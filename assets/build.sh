0eb1317bea810f58962a4b46204f263704f42fcb
conni <cbuschka@gmail.com>
2021-05-22 22:26:17 +0200

    Added branch checkout and updating/synching existing repo.

--- assets/build.sh
+++ assets/build.sh
@@ -6,15 +6,21 @@ set -e
 if [ ! -d "/build/depot_tools" ]; then
   cd /build
   git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
+  git checkout -b master -t origin/master
 else
   cd /build/depot_tools
+  git checkout master
   git pull
 fi
 export PATH=$PATH:/build/depot_tools
 
 mkdir -p /build/dart-sdk
 cd /build/dart-sdk
-fetch dart
+if [ ! -d "/build/dart-sdk/sdk" ]; then
+  fetch dart
+else
+  gclient sync
+fi
 cd /build/dart-sdk/sdk
 ./tools/build.py --no-goma --mode release --arch x64 create_sdk
 
